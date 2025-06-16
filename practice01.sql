9e450c658e760495e28058ae8e7f086c1a3354cf
song373 <song120437@naver.com>
2025-06-16 17:03:56 +0900

    연습1 기본 쿼리

---
+++ practice01.sql
@@ -0,0 +1,24 @@
+--1.
+select * from student;
+
+select name id, weight  from student;
+
+select
+name || '''s ID : ' || id || ', WEIGHT is ' || weight  || 'KG' AS " ID AND WEIGHT"
+from student;
+
+
+--2.
+select ename, ename, ename, ename from emp;
+select ename, job from emp;
+
+select ename || '(' || job || ') , ' || ename || '''' || job || '''' "NAME AND JOB"
+from emp;
+
+--3.
+select * from emp;
+select ename, sal from emp;
+
+select ename || '''s sal is $' || sal AS "NAME and Sal" 
+from emp;
+
